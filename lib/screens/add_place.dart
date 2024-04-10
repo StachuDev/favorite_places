78d57adfb90a0dc03c2bc1b2636d0eae3f28ac28
StachuDev <solszakdev@gmail.com>
2024-04-10 22:44:24 +0200

    flutter location instalation and config - update kotlin verion, locationInput widget

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:favorite_places/models/place.dart';
 import 'package:favorite_places/providers/places_provider.dart';
 import 'package:favorite_places/widgets/image_input.dart';
+import 'package:favorite_places/widgets/location_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -74,6 +75,8 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
             ),
             const SizedBox(height: 10),
             ImageInput(onPickImage: (image) => _selectedImage = image),
+            const SizedBox(height: 16),
+            const LocationInput(),
             const SizedBox(height: 16),
             ElevatedButton.icon(
               onPressed: onSubmitPlace,
