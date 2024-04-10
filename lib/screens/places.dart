5b6f21fbfb37a67acd9b1dccd2b2ce591312e958
StachuDev <solszakdev@gmail.com>
2024-04-10 12:56:35 +0200

    text styling, change modalBottomSheet to navigator.push function, change stateful to stateless widget

--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -5,26 +5,20 @@ import 'package:favorite_places/screens/place_details.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class PlacesScreen extends ConsumerStatefulWidget {
+class PlacesScreen extends ConsumerWidget {
   const PlacesScreen({super.key});
+  // void openAddPlaceOverlay() {
+  //   showModalBottomSheet(
+  //       constraints: const BoxConstraints(
+  //         maxWidth: double.infinity,
+  //         minHeight: double.infinity,
+  //       ),
+  //       useSafeArea: true,
+  //       context: context,
+  //       builder: (ctx) => const AddPlaceScreen());
+  // }
 
-  @override
-  ConsumerState<PlacesScreen> createState() => _PlacesScreenState();
-}
-
-class _PlacesScreenState extends ConsumerState<PlacesScreen> {
-  void openAddPlaceOverlay() {
-    showModalBottomSheet(
-        constraints: const BoxConstraints(
-          maxWidth: double.infinity,
-          minHeight: double.infinity,
-        ),
-        useSafeArea: true,
-        context: context,
-        builder: (ctx) => const AddPlaceScreen());
-  }
-
-  void selectPlace(PlaceItem place) {
+  void selectPlace(PlaceItem place, BuildContext context) {
     Navigator.push(
       context,
       MaterialPageRoute(
@@ -34,22 +28,32 @@ class _PlacesScreenState extends ConsumerState<PlacesScreen> {
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final places = ref.watch(placesProvider);
 
     Widget content = ListView.builder(
       itemCount: places.length,
       itemBuilder: (ctx, index) => ListTile(
-        title: Text(places[index].title),
+        title: Text(
+          places[index].title,
+          style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
         onTap: () {
-          selectPlace(places[index]);
+          selectPlace(places[index], context);
         },
       ),
     );
 
     if (places.isEmpty) {
-      content = const Center(
-        child: Text("Nie dodano żadnych miejsc"),
+      content = Center(
+        child: Text(
+          "Nie dodano żadnych miejsc",
+          style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
       );
     }
 
@@ -58,7 +62,14 @@ class _PlacesScreenState extends ConsumerState<PlacesScreen> {
         title: const Text('Lista ulubionych miejsc: '),
         actions: [
           IconButton(
-              onPressed: openAddPlaceOverlay, icon: const Icon(Icons.add))
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (ctx) => const AddPlaceScreen(),
+                  ),
+                );
+              },
+              icon: const Icon(Icons.add))
         ],
       ),
       body: content,
